e4d57d5417e91342e25ec22474a00da0b7f2f292
vihiolet <tlsdbswls77@naver.com>
2023-05-23 00:26:14 +0900

    keyword start

---
+++ project/on/src/vo/KeywordBean.java
@@ -0,0 +1,36 @@
+package vo;
+
+import java.sql.Date;
+
+public class KeywordBean {
+	private int srch_code;
+	private String srch_name;
+	private Date create_dt;
+	private int create_id;
+	
+	public int getSrch_code() {
+		return srch_code;
+	}
+	public void setSrch_code(int srch_code) {
+		this.srch_code = srch_code;
+	}
+	public String getSrch_name() {
+		return srch_name;
+	}
+	public void setSrch_name(String srch_name) {
+		this.srch_name = srch_name;
+	}
+	public Date getCreate_dt() {
+		return create_dt;
+	}
+	public void setCreate_dt(Date create_dt) {
+		this.create_dt = create_dt;
+	}
+	public int getCreate_id() {
+		return create_id;
+	}
+	public void setCreate_id(int create_id) {
+		this.create_id = create_id;
+	}
+	
+}
