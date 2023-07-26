1c8482f81731c72bfe574eb1ebfabfef67631abd
vihiolet <tlsdbswls77@naver.com>
2023-07-26 17:38:13 +0900

    Create UserQuitFormAction.java

---
+++ project/on/src/action/UserQuitFormAction.java
@@ -0,0 +1,26 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.ActionForward;
+
+public class UserQuitFormAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    
+		ActionForward forward= new ActionForward();		
+		HttpSession session= request.getSession();
+		String id= (String)session.getAttribute("id");
+		UserListService userListService= null;
+		UserBean userInfo= null;
+		
+		userListService= new UserListService();
+		userInfo= userListService.getUserInfo2(id);
+		request.setAttribute("userInfo", userInfo);
+    
+		return forward;
+	}
+
+}
