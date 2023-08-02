506c7d6d06ec87218bc1e29be4a099ca5e9849cc
vihiolet <tlsdbswls77@naver.com>
2023-08-02 14:03:10 +0900

    Create UserReviewService.java

---
+++ project/on/src/svc/UserReviewService.java
@@ -0,0 +1,33 @@
+package svc;
+import static db.JdbcUtil.*;
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import dao.UsersDAO;
+import vo.UserBean;
+
+public class UserListService {
+
+	public UserBean getUserInfo2(String id) {
+		UsersDAO userDAO= UsersDAO.getInstance();
+		Connection con= getConnection();
+		userDAO.setConnection(con);
+		
+		UserBean userInfo= new UserBean();
+		userInfo= userDAO.selectUserInfo2(id);
+		
+		close(con);		
+		return userInfo;
+	}
+  //내가 투표한 리뷰
+  public ArrayList getMyReview(){
+    UsersDAO userDAO= UsersDAO.getInstance();
+		Connection con= getConnection();
+		userDAO.setConnection(con);
+    
+    ArrayList myReview= new ArrayList();
+		myReview= userDAO.getMyReview();
+    close(con);		
+		return myReview;
+  }
+}
